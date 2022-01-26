dbb8e0148bf8c984c62dc8b9aed121e7a4cc07a2
Rotiken Gisa <shadrqen@outlook.com>
2022-01-26 23:19:37 +0300

    test: add vuetify and comment out warnings

--- client/tests/unit/navbar.spec.js
+++ client/tests/unit/navbar.spec.js
@@ -1,51 +1,69 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils'
 import Vuex from 'vuex'
+import Vuetify from 'vuetify'
 import NavBar from '@/components/general/NavBar.vue'
 
 const localVue = createLocalVue()
 
-localVue.use(Vuex)
-
-const $store = {
-  state: {
-    user: {
-      loginStatus: true
-    },
-    design: {
-      viewport_code: 'xs'
-    }
-  }
-}
+// const $vuetify = {
+//   breakpoint: {
+//     name: 'lg'
+//   }
+// }
 
 // eslint-disable-next-line no-undef
 describe('It shows the correct items on hero image', () => {
   let actions, getters, store, wrapper
+  // eslint-disable-next-line no-undef
   beforeEach(() => {
+    const breakpoint = {
+      // eslint-disable-next-line no-undef
+      init: jest.fn(),
+      framework: {},
+      smAndDown: true
+    }
+    const $vuetify = new Vuetify()
+    $vuetify.framework.breakpoint = breakpoint
+
+    const $store = {
+      state: {
+        user: {
+          loginStatus: true
+        },
+        design: {
+          viewport_code: 'xs'
+        }
+      }
+    }
+    localVue.use(Vuex)
+    getters = {
+      loginStatus: () => true
+    }
     actions = {
+      // eslint-disable-next-line no-undef
       changeLoginDialog: jest.fn()
     }
     store = new Vuex.Store({
-      actions
+      actions,
+      getters
     })
     /* wrapper = shallowMount(NavBar, {
       store,
       localVue,
+      $vuetify,
       mocks: {
         $store
       }
     }) */
-    actions = {
-      actionClick: jest.fn(),
-      actionInput: jest.fn()
-    }
-    store = new Vuex.Store({
-      actions
-    })
   })
-  it('not yet finished a test', () => {
+  // eslint-disable-next-line no-undef
+  it('sample test', () => {
+    // eslint-disable-next-line no-undef
     expect(1).toBe(1)
   })
-  // it('renders hero image', () => {
-  //   expect(wrapper.exists()).toBe(true)
-  // })
+/*  // eslint-disable-next-line no-undef
+  it('renders hero image', () => {
+    // eslint-disable-next-line no-undef
+    expect(wrapper.exists()).toBe(true)
+  }) */
 })
